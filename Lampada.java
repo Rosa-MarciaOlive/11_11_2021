1db024a0c254f7959a8687dffecf3bd52dc532de
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-11-11 17:30:51 +0000

    Lâmpada

---
+++ Lampada.java
@@ -0,0 +1,22 @@
+public class Lampada{
+    //private pr usar o encapsulamento
+    private boolean situacaoLampada;
+
+    public boolean isSituacaoLampada() {
+        return situacaoLampada;
+    }
+
+    public void setSituacaoLampada(boolean situacaoLampada) {
+        this.situacaoLampada = situacaoLampada;
+    }
+
+    public void ligar(){
+        situacaoLampada=true;
+    }
+    public void desligar(){
+        situacaoLampada=false;
+    }
+    
+    
+    
+}
---
+++ TesteLampada.java
@@ -0,0 +1,13 @@
+public class TesteLampada {
+
+    public static void main(String[] args) {
+        //criar objeto
+        Lampada l=new Lampada();
+        l.ligar();
+
+        if(l.isSituacaoLampada()){
+            System.out.println("Lampada ligada");
+        }else
+            System.out.println("Lampada desligada");
+    }
+}
